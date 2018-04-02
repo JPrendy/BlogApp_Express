59b072cac1ca3104d8d76e70e31b786cc28d27e4
James Prendergast <jamprendergast@gmail.com>
2018-04-02 19:01:20 +0100

    the addition of edit and delete, plus a better ui

--- RestfulBlogApp/app.js
+++ RestfulBlogApp/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var methodOverride = require("method-override");
 bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
@@ -9,7 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 
 //create the schema for MongoDB
 var blogSchema = new mongoose.Schema({
@@ -45,10 +46,25 @@ app.get("/blogs", function(req, res){
     }).sort({_id:-1}) ; //this will sort the post by latest insertion into the DB
 });
 
+//Edit Route
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, respBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+          res.render("edit", {blog: respBlog});
+        }
+    });
+});
+
+
+
+
 //New Route
 app.get("/blogs/new", function(req, res){
     res.render("new");
-})
+});
 
 
 //Create the New Route
@@ -75,6 +91,31 @@ app.get("/blogs/:id", function(req,res){
        }
     });
 });
+
+//Update Route
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, respUpdate){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//Delete Route
+//the delete is called from show.ejs when the delete button is pressed.
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs"); 
+        }
+    });
+});
+
 
 app.listen(3000, function(){
     console.log("server is running");
